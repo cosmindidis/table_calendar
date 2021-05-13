a3ea274805bbff74f892713b19cad7aa3aa3b298
Alexandra Mindrean <alexandra.mindrean@keez.ro>
2021-05-13 13:22:49 +0300

    test push

--- lib/table_calendar.dart
+++ lib/table_calendar.dart
@@ -7,4 +7,4 @@ export 'src/customization/days_of_week_style.dart';
 export 'src/customization/header_style.dart';
 export 'src/shared/utils.dart';
 export 'src/table_calendar.dart';
-export 'src/table_calendar_base.dart';
+export 'src/table_calendar_base.dart';  
